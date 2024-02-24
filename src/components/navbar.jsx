d261c7834ccb3bf77875d46f4987277d67d6531e
molhamalrashed <molham.alrd@gmail.com>
2024-02-24 23:13:08 +0100

    menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,12 @@
 import Link from 'next/link';
 
+const links = [
+{url: '/', title: 'Home'},
+{url: '/about', title: 'About'},
+{url: '/portfolio', title: 'Portfolio'},
+{url: '/contact', title: 'Contact'},
+];
+
 const Navbar = () => {
 return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
@@ -12,7 +19,19 @@ return (
     </div>
 
     {/* Responsive menu */}
-    <div>2</div>
+    <div>
+        <button className = 'w-10 h-8 flex flex-col justify-between'>
+            <span className='w-10 h-1 bg-black rounded'></span>
+            <span className='w-10 h-1 bg-black rounded'></span>
+            <span className='w-10 h-1 bg-black rounded'></span>
+        </button>
+        {/* Menu list */}
+        <div>
+            {links.map((link)=>(
+                <Link href={link.url} key={link.url}>{link.title}</Link>
+            ))}
+        </div>
+    </div>
     </div>
 )
 }
