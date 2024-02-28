8e1bf423d05c7dfb1efd7b005622dc95c6d430f4
molhamalrashed <molham.alrd@gmail.com>
2024-02-28 20:43:55 +0100

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return <div className="h-full flex flex-col">
+    {/* Image Section */}
+    <div className="h-1/2 relative">
+      <Image src="/hero.png" alt="Molham" fill className='object-contain' />
+    </div>
+    {/* Text section */}
+    <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+      <h1 className='text-4xl font-bold'>Full-Stack Web Developer</h1>
+      <p>As a passionate and dedicated web developer, I am committed to continuous learning and self-improvement. I thrive in a dynamic team environment and possess strong organizational and time-management skills. With a background in teaching and voice acting, my excellent communication and interpersonal skills allow me to effectively collaborate with clients and colleagues.</p>
+    
+    {/* Buttons */}
+    <div className='flex gap-8'>
+      <button className='p-4 rounded-lg ring-1 ring-black bg-black text-white'>View my work</button>
+      <button className='p-4 rounded-lg ring-1 ring-black'>Contact me</button>
+    </div>
+    </div>
+  </div>;
 };
 
 export default Homepage;
