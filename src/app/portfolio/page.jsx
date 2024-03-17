5aebc2325281b619f37e55b323e6225e1a73d962
molhamalrashed <molham.alrd@gmail.com>
2024-03-17 11:28:41 +0100

    hire me

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -72,14 +72,19 @@ return (
                 <div className='w-screen h-screen flex flex-col gap-16 items-center justify-center text-center'>
                   <h1 className='text-black'>Do you have a project</h1>
                   <div className='relative'>
-                    <svg viewBox='0 0 300 300'>
+                    <svg viewBox='0 0 300 300' className='w-64 h-64 md:w-[500px] md:h-[500px]'>
                       <defs>
                         <path id='circlePath'
                         d='M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0'>
                         </path>
                       </defs>
+                      <text fill="#000">
+                  <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end & Back-end  Web-Developer
+              </textPath>
+            </text>
                     </svg>
-                    <Link href="/contact" className='w-16 h-16 md:w-24 md:h-24 absolute top-0 right-0 left-0 bottom-0 m-auto bg-black text-white rounded-full'>Hire me</Link>
+                    <Link href="/contact" className='w-16 h-16 md:w-24 md:h-24 absolute top-0 right-0 left-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center'>Hire me</Link>
                   </div>
         </div>
     </motion.div>
