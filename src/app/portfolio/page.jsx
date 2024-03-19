ed632382d6e538c3288903576ee7b88e671851d2
molhamalrashed <molham.alrd@gmail.com>
2024-03-19 21:21:48 +0100

    projects

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import {useRef} from "react";
 import {motion, useTransform, useScroll} from "framer-motion";
-import { Linefont } from 'next/font/google';
 
 const items = [
     {
@@ -56,23 +55,25 @@ return (
             <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-red-100 to-blue-200" />
             {items.map((item) => (
               <div
-                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+                className={`h-screen w-screen flex pt-[10rem] md:pt-[5rem] lg:pt-[5rem] xl:pt-[3rem] justify-center bg-gradient-to-r ${item.color}`}
                 key={item.id}
               >
                     <div className='flex flex-col relative text-white gap-8'>
-                        <h1>{item.title}</h1>
-                        <div >
+                        <h1 className='flex text-center font-bold text-2xl md:text-4xl lg:text-6xl xl:text-8xl'>{item.title}</h1>
+                        <div className='relative w-80 h-60 md:w-[400px] md:h-[280px] lg:w-[500px] lg:h-[350px]  '>
                         <Image src={item.image} alt={item.title} fill/>
                         </div>
-                        <p className='text-black'>{item.description}</p>
-                        <Link href={item.link}><button className='bg-black text-white'>Demo</button></Link>
+                        
+                        <p className='text-[#392775]  w-80 md:w-[400px] lg:w-[500px] xl:w-[600px] lg:text-lg'>{item.description}</p>
+                        <Link href={item.link} target='_blank' className='flex justify-end '><button className='rounded-lg bg-[#38296b] p-3 m-3'>Demo</button></Link>
+                        
                     </div>
                 </div>))}
                 </motion.div>
             </div>
                 </div>
                 <div className='w-screen h-screen flex flex-col gap-16 items-center justify-center text-center'>
-                  <h1 className='text-black text-8xl'>Do you have a project</h1>
+                  <h1 className='text-black text-8xl pt-[10rem]'>Do you have a project</h1>
                   <div className='relative'>
                     <motion.svg animate={{rotate:-360}} transition={{duration:8, ease:"linear", repeat:Infinity}} viewBox='0 0 300 300' className='w-64 h-64 md:w-[500px] md:h-[500px]'>
                       <defs>
